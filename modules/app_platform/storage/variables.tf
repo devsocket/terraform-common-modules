4ed804f7111f4191fafbd7913b08be65fcf808f0
devsocket <venkatasudheerkumar.kondeti@gmail.com>
2026-04-09 22:00:35 -0300

    fix: correct regex for storage account name validation and update resource group output reference

--- modules/app_platform/storage/variables.tf
+++ modules/app_platform/storage/variables.tf
@@ -13,7 +13,7 @@ variable "storage_account_name" {
   description = "Storage account name to interact with. should be unique globally. 3-24 chars, lowercase alpha numerics only."
 
   validation {
-    condition     = can(regex("^[a-z0-9]{3-24}$", var.storage_account_name))
+    condition     = can(regex("^[a-z0-9]{3,24}$", var.storage_account_name))
     error_message = "Storage account must be of 3-35 lowercase alpha numerics only."
   }
 }
