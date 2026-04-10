d9d97dc7b9cb42bf2b0c779b41a7ec810cb9fd0d
devsocket <venkatasudheerkumar.kondeti@gmail.com>
2026-04-09 22:18:19 -0300

    fix: update resource_group_name references in private_dns module

--- modules/connectivity/private_dns/main.tf
+++ modules/connectivity/private_dns/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_private_dns_zone" "this" {
   for_each = toset(var.dns_zones)
 
   name                = each.key
-  resource_group_name = var.resource_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
 
   tags = var.tags
 }
@@ -27,7 +27,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "hub" {
   for_each = toset(var.dns_zones)
 
   name                  = "${var.hub_vnet_link_name}-${each.key}"
-  resource_group_name   = var.resource_group.this.name
+  resource_group_name   = azurerm_private_dns_zone.this.name
   private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
   virtual_network_id    = var.hub_vnet_id
   registration_enabled  = var.enable_auto_registration
